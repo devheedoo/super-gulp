2c78f6af677dff08c9e963fb9504bf5e9b5c20fc
Choco Kim <devheedoo@gmail.com>
2022-01-15 08:49:01 +0900

    feat: Watch changes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,7 +5,8 @@ import ws from 'gulp-webserver';
 
 const routes = {
   pug: {
-    src: 'src/**/*.pug',
+    watch: 'src/**/*.pug',
+    src: 'src/*.pug',
     dest: 'build',
   },
 };
@@ -18,9 +19,12 @@ const clean = () => del(['build']);
 const webserver = () =>
   gulp.src('build').pipe(ws({ livereload: true, open: true }));
 
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+};
 const prepare = gulp.series([clean]);
 const assets = gulp.series([pug]);
-const postDev = gulp.series([webserver]);
+const postDev = gulp.parallel([webserver, watch]);
 
 export const dev = gulp.series([prepare, assets, postDev]);
 
